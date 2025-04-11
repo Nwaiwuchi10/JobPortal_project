4fc41443f4925ecd40fc121f722bcc388d050615
Nwaiwuchi10 <djnchrys@gmail.com>
2025-04-11 11:34:28 -0700

    new

--- src/components/LatestJobs.jsx
+++ src/components/LatestJobs.jsx
@@ -27,8 +27,11 @@ const LatestJobs = () => {
    
     return (
         <div className='max-w-7xl mx-auto my-20'>
-            <h1 className='text-4xl font-bold'><span className='text-[#6A38C2]'>Latest & Top </span> Job Openings</h1>
-            <div className='grid grid-cols-3 gap-4 my-5 data-div-display'>
+            <h1 className="text-3xl font-bold text-center mb-10 md:text-3xl lg:text-4xl"><span className='text-[#6A38C2]'>Latest & Top </span> Job Openings</h1>
+            <div 
+            className='data-div-display'
+            // className='grid grid-cols-3 gap-4 my-5 data-div-display'
+            >
                 {
                     allJobs.length <= 0 ? <span>No Job Available</span> : allJobs?.slice(0,6).map((job) => <LatestJobCards key={job._id} job={job}/>)
                 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
     screens: {
       sm: "480px",
       md: "768px",
+      lg:"1024px"
       // etc.
     },
     container: {
